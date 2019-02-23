859272ff8afa8e94e5c06ac160a776fd48f4ea69
True Ghiassi <true@ghiassitrio.co.uk>
2019-02-23 19:45:10 +0000

    The hello world server and Elm frontend are working.

--- Main.elm
+++ Main.elm
@@ -1,50 +1,77 @@
+module Main exposing (main)
+
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes as Hat
+import Html.Events exposing (onClick, onInput)
+import Http
 import Url
 
+
 main =
-  Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlRequest = \_ -> Increment
-    , onUrlChange = \_ -> Increment
-    }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , onUrlRequest = \_ -> DoNothing
+        , onUrlChange = \_ -> DoNothing
+        }
 
-subscriptions _ =
-    listen "ws://localhost:3000" FromServer
 
-init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
-init _ _ _ = ({boxStr = "", displayStr = ""}, Cmd.none)
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( { boxStr = "", displayStr = "" }, Cmd.none )
+
 
 type Msg
     = TypedIn String
-    | FromServer String
- 
-type alias Model = 
+    | FromServer (Result Http.Error String)
+    | DoNothing
+
+
+type alias Model =
     { boxStr : String
     , displayStr : String
     }
 
+
+postMsg : String -> Cmd Msg
+postMsg txt =
+    Http.post
+        { url = "http://localhost:3000"
+        , expect = Http.expectString FromServer
+        , body = Http.stringBody "text/plain; charset=utf-8" txt
+        }
+
+
 update msg model =
-  case msg of
-    TypeIn txt ->
-      ( { model | boxStr = txt }, send "ws://localhost:3000" txt )
-    FromServer str ->
-      { model | displayStr = str }
+    case msg of
+        TypedIn txt ->
+            ( { model | boxStr = txt }, postMsg txt )
+
+        FromServer (Err err) ->
+            ( model, Cmd.none )
+
+        FromServer (Ok str) ->
+            ( { model | displayStr = str }, Cmd.none )
+
+        DoNothing ->
+            ( model, Cmd.none )
+
 
 view model =
-  { title = "BigWebThing"
-  , body = [
-      div []
-        [ input
-            [ placeholder "Text to send"
-            , value model.boxStr
-            , onInput TypedIn
-            ] []
+    { title = "BigWebThing"
+    , body =
+        [ div []
+            [ input
+                [ Hat.placeholder "Text to send"
+                , Hat.value model.boxStr
+                , onInput TypedIn
+                ]
+                []
+            ]
+        , text model.displayStr
         ]
-    ]
-  }
+    }
