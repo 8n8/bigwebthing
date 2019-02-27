bc696571beb7f8044ddd3799398864ffb67181f1
True Ghiassi <true@ghiassitrio.co.uk>
2019-02-27 09:23:18 +0000

    My hello world Go program is working with the hello world Elm program now.

--- go/src/bwt/bwt.go
+++ go/src/bwt/bwt.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		f := http.FileServer(http.Dir("/home/t/bigwebthing"))
+		f.ServeHTTP(w, r)
+	case "POST":
+		bodyBytes, _ := ioutil.ReadAll(r.Body)
+		bodyStr := string(bodyBytes)
+		fmt.Fprintf(w, bodyStr + " world!")
+	}
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/home/t/bigwebthing")))
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 }
