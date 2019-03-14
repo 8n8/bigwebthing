a658e340b76626357859b13da083f4dd0844a61f
True Ghiassi <true@ghiassitrio.co.uk>
2019-03-14 20:09:54 +0000

    Made search box and main buttons for home page of main app.

--- Main.elm
+++ Main.elm
@@ -2,16 +2,17 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Nav
+import Element as E
+import Element.Background as Bg
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Ei
 import Html exposing (Html, button, div, input, text)
 import Html.Attributes as Hat
 import Html.Events exposing (onClick, onInput)
 import Http
+import List as L
 import Url
-import Element as E -- exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
-import Element.Input as Ei
-import Element.Background as Background
-import Element.Border as Border
-import Element.Font as Font
 
 
 main =
@@ -33,6 +34,8 @@ init _ _ _ =
 type Msg
     = TypedIn String
     | FromServer (Result Http.Error String)
+    | NewDocumentButtonClick
+    | MembershipButtonClick
     | DoNothing
 
 
@@ -65,20 +68,120 @@ update msg model =
         DoNothing ->
             ( model, Cmd.none )
 
+        MembershipButtonClick ->
+            ( model, Cmd.none )
+
+        NewDocumentButtonClick ->
+            ( model, Cmd.none )
+
 
 view model =
     { title = "BigWebThing"
     , body = [ E.layout [] (mainEl model) ]
     }
 
+
+placeholder : Ei.Placeholder Msg
+placeholder =
+    Ei.placeholder [] <| E.text "Type here to search"
+
+
+black =
+    E.rgb 0 0 0
+
+
+searchStyle =
+    [ Border.width 1
+    , Border.color black
+    , Border.solid
+    , Border.rounded 0
+    , E.width <| E.px 600
+
+    --, E.padding 0
+    ]
+
+
+edges =
+    { top = 0, left = 0, right = 0, bottom = 0 }
+
+
+createNowStyle =
+    [ E.moveDown 1
+    , E.alignLeft
+    , E.alignTop
+    , E.paddingEach { edges | top = 33 }
+    ]
+
+
+idPadding =
+    { top = 5
+    , right = 20
+    , bottom = 0
+    , left = 0
+    }
+
+
+idtxt =
+    [ "8XKTUnNufTH"
+    , "mkEqNv0zxI7"
+    , "z/+npv+RPpD"
+    , "vm7HyN7zc0="
+    ]
+
+
+myId =
+    E.column
+        [ Font.family [ Font.typeface "Courier" ]
+        , Font.size 20
+        , E.alignRight
+        , E.alignTop
+        , E.paddingEach idPadding
+        ]
+    <|
+        (E.el [ Font.bold ] <| E.text "Public ID:")
+            :: L.map E.text idtxt
+
+
+searchBoxStyle =
+    [ Font.family [ Font.typeface "Courier", Font.monospace ]
+    , E.paddingEach
+        { top = 20
+        , left = 20
+        , right = 0
+        , bottom = 0
+        }
+    , E.alignTop
+    ]
+
+
 mainEl : Model -> E.Element Msg
 mainEl model =
-    E.column []
-        [ Ei.text []
-            { onChange = TypedIn
-            , text = model.boxStr
-            , placeholder = Nothing
-            , label = Ei.labelAbove [] E.none
-            }
-        , E.text model.displayStr
+    E.column
+        [ Font.family [ Font.typeface "Georgia", Font.serif ]
+        , Font.size 28
+        , E.width E.fill
+        ]
+        [ E.row [ E.width E.fill, E.spacing 30 ]
+            [ E.el searchBoxStyle <|
+                Ei.text
+                    searchStyle
+                    { onChange = TypedIn
+                    , text = model.boxStr
+                    , placeholder = Just placeholder
+                    , label = Ei.labelAbove [] E.none
+                    }
+            , E.el createNowStyle <|
+                Ei.button
+                    []
+                    { onPress = Just NewDocumentButtonClick
+                    , label = E.text "New document"
+                    }
+            , E.el createNowStyle <|
+                Ei.button
+                    []
+                    { onPress = Just MembershipButtonClick
+                    , label = E.text "Members"
+                    }
+            , myId
+            ]
         ]
