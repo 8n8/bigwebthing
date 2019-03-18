01f7f732bc5651280567e32948f7879dab1df413
True Ghiassi <true@ghiassitrio.co.uk>
2019-03-18 22:27:46 +0000

    Set up main structure of client, with a main state-managing loop and the http server in its own goroutine.

--- go/src/client/client.go
+++ go/src/client/client.go
@@ -6,6 +6,61 @@ import (
 	"net/http"
 )
 
+type inputT interface {
+	update(*stateT) (stateT, outputT)
+}
+
+type outputT interface {
+	send() inputT
+}
+
+type stateT struct {
+	httpChans httpChansT
+}
+
+type httpChansT struct {
+	in chan []byte
+	out chan []byte
+}
+
+func (h httpChansT) send() inputT {
+	select {
+	case body := <-h.in:
+		return httpInputT{postBody: body}
+	default:
+	}
+	return noInputT{}
+}
+
+func (h httpInputT) update(s *stateT) (stateT, outputT) {
+	return stateT{}, httpChansT{}
+}
+
+type httpInputT struct {
+	postBody []byte
+}
+
+type noInputT struct{}
+
+func (n noInputT) update(s *stateT) (stateT, outputT) {
+	return *s, s.httpChans
+}
+
+func main() {
+	var state stateT
+	// err := readFileData(&state)
+	// if err != nil {
+	// 	return
+	// }
+	go httpServer(state.httpChans)
+	var input inputT = noInputT{}
+	var output outputT = state.httpChans
+	for {
+		input = output.send()
+		state, output = input.update(&state)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
@@ -21,7 +76,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
+func httpServer(ch httpChansT) {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 }
