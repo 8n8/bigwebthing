60f5cacf05c58a1f8a1d097ad77c59db6634ab1a
True Ghiassi <true@ghiassitrio.co.uk>
2019-05-28 18:01:34 +0100

    Changed author argument in Decrypted interface 'process' function from [32]byte to publicSignT.

--- go/src/client/client.go
+++ go/src/client/client.go
@@ -568,7 +568,7 @@ type sendAppMsgT struct {
 	recipient publicSignT
 }
 
-func (appSig appMsgT) process(author [32]byte, s *stateT) stateT {
+func (appSig appMsgT) process(author publicSignT, s *stateT) stateT {
 	fmt.Println("Top of appMsgT process function.")
 	newChunksLoading := make(map[blake2bHash][]fileChunkPtrT)
 	for appHash, chunkPtr := range s.chunksLoading {
@@ -796,7 +796,7 @@ func sendChunkNew(state *stateT, s sendChunkT) stateT {
 }
 
 type Decrypted interface {
-	process([32]byte, *stateT) stateT
+	process(publicSignT, *stateT) stateT
 }
 
 type ReceiptT struct {
@@ -989,13 +989,14 @@ func decodeMsg(bs []byte) (Decrypted, error) {
 }
 
 func (appReceipt AppReceiptT) process(
-	author [32]byte,
+	author publicSignT,
 	s *stateT) stateT {
 
+	bytesAuthor := [32]byte(author)
 	signed, ok := sign.Open(
 		make([]byte, 0),
 		common.SigToSlice(appReceipt.Sig),
-		&author)
+		&bytesAuthor)
 	if !ok {
 		return *s
 	}
@@ -1020,7 +1021,7 @@ func (appReceipt AppReceiptT) process(
 	return newS
 }
 
-func (receipt ReceiptT) process(author [32]byte, s *stateT) stateT {
+func (receipt ReceiptT) process(author publicSignT, s *stateT) stateT {
 	fmt.Println("The receipt is:")
 	fmt.Println(receipt)
 	chunkAwaiting, ok := s.chunksAwaitingReceipt[receipt.ChunkHash]
@@ -1455,7 +1456,7 @@ func processTcpInput(s stateT, c common.ClientToClient) stateT {
 	return s
 }
 
-func (chunk FileChunk) process(author [32]byte, s *stateT) stateT {
+func (chunk FileChunk) process(author publicSignT, s *stateT) stateT {
 	fmt.Println("chunk.Counter:")
 	fmt.Println(chunk.Counter)
 	previousChunks, ok := s.chunksLoading[chunk.AppHash]
