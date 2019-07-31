800c7cfada2af074613d2f65ac41eb3e64c8cd07
True Ghiassi <true@ghiassitrio.co.uk>
2019-07-31 22:46:00 +0100

    Adds app address input box and app install button.

--- Main.elm
+++ Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (main)
 
+import Base64
 import Browser
 import Browser.Navigation as Nav
 import Element as E
 import Element.Font as Font
+import Element.Input as Ei
 import Http
 import Time
 import Url
@@ -29,23 +31,61 @@ parseRoute =
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( { securityCode = Up.parse parseRoute url }, Cmd.none )
+    ( { securityCode = Up.parse parseRoute url
+      , installBox = ""
+      , installErr = Nothing
+      }
+    , Cmd.none
+    )
 
 
 type Msg
     = LaunchApp String
     | AppLaunched (Result Http.Error ())
     | DoNothing
+    | InstallBox String
+    | PressInstallButton
+    | Installed (Result Http.Error ())
 
 
 type alias Model =
     { securityCode : Maybe String
+    , installBox : String
+    , installErr : Maybe Http.Error
     }
+
+
+installApp : String -> String -> Cmd Msg
+installApp code url =
+    Http.post
+        { url = "http://localhost:3000/install/" ++ code
+        , body = Http.stringBody "text/plain" url
+        , expect = Http.expectWhatever Installed
+        }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Installed (Ok ()) ->
+            ( model, Cmd.none )
+
+        Installed (Err err) ->
+            ( { model | installErr = Just err }, Cmd.none )
+
+        PressInstallButton ->
+            ( model
+            , case model.securityCode of
+                Nothing ->
+                    Cmd.none
+
+                Just code ->
+                    installApp code model.installBox
+            )
+
+        InstallBox txt ->
+            ( { model | installBox = txt }, Cmd.none )
+
         DoNothing ->
             ( model, Cmd.none )
 
@@ -90,6 +130,27 @@ badSecurityCode =
             ++ "not read the security code from the URL."
 
 
+prettyHttpError : Http.Error -> String
+prettyHttpError err =
+    "Error installing app: "
+        ++ (case err of
+                Http.BadUrl str ->
+                    "bad url: " ++ str
+
+                Http.Timeout ->
+                    "timeout"
+
+                Http.NetworkError ->
+                    "network error"
+
+                Http.BadStatus code ->
+                    "bad status code: " ++ String.fromInt code
+
+                Http.BadBody str ->
+                    "bad response body: " ++ str
+           )
+
+
 goodSecurityCode : Model -> E.Element Msg
 goodSecurityCode model =
     E.column
@@ -97,5 +158,36 @@ goodSecurityCode model =
         , E.padding 5
         , E.spacing 5
         ]
-        [ E.text "hello"
+    <|
+        [ installBox model.installBox
+        , installButton
         ]
+            ++ (case model.installErr of
+                    Nothing ->
+                        []
+
+                    Just err ->
+                        [ E.text <| prettyHttpError err ]
+               )
+
+
+installButton : E.Element Msg
+installButton =
+    Ei.button []
+        { onPress = Just PressInstallButton
+        , label = E.text "Install app"
+        }
+
+
+installBox : String -> E.Element Msg
+installBox txt =
+    Ei.text []
+        { onChange = InstallBox
+        , text = txt
+        , placeholder =
+            Just <|
+                Ei.placeholder [] <|
+                    E.text "Enter address of app to install"
+        , label =
+            Ei.labelHidden "Enter address of app to install:"
+        }
