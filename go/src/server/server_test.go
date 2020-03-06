510db73da0fcbc945cf9c37e9eb408a5bea4c56b
True Ghiassi <true@ghiassitrio.co.uk>
2020-03-06 11:53:35 +0000

    Added some more tests.

--- go/src/server/server_test.go
+++ go/src/server/server_test.go
@@ -3,22 +3,56 @@ package main
 import (
 	"testing"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/gofuzz"
+	"encoding/binary"
 )
 
 
 func TestMakeFriendlyNameParserSimpleOk(t *testing.T) {
-	request := append([]byte{1}, make([]byte, 56)...)
+	powServer := []byte{2,2,2,2,2,2,2,2}
+	powClient := []byte{3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}
+	powflat := append(powServer, powClient...)
+	newKey := make([]byte, 32)
+	for i, _ := range newKey {
+		newKey[i] = 4
+	}
+	request := append(append([]byte{1}, powflat...), newKey...)
 	pow := proofOfWorkT{
-		Server: make([]byte, 8),
-		Client: make([]byte, 16),
+		Server: powServer,
+		Client: powClient,
 	}
 	expected := makeFriendlyNameRequest{
 		ProofOfWork: pow,
-		NewKey: make([]byte, 32),
+		NewKey: newKey,
 	}
 	actual := parseRequest(request)
 	diff := cmp.Diff(expected, actual)
 	if diff != "" {
 		t.Errorf("parseRequest (make friendly name): %v", diff)
+	}
+}
+
+func TestMakeFriendlyNameParserFuzz(t *testing.T) {
+	f := fuzz.New()
+	for i := 0; i < 100; i++ {
+		var requestMost []byte
+		f.Fuzz(&requestMost)
+		parseRequest(append([]byte{1}, requestMost...))
+	}
+}
+
+func TestLookupNameSimple(t *testing.T) {
+	f := fuzz.New()
+	for i := 0; i < 100; i++ {
+		var name uint64
+		f.Fuzz(&name)
+		buf := make([]byte, 8)
+		_ = binary.PutUvarint(buf, name)
+		request := append([]byte{2}, buf...)
+		actual := parseRequest(request)
+		expected := lookupNameT(name)
+		if actual != expected {
+			t.Errorf("expected %v, but got %v", expected, actual)
+		}
 	}
 }
