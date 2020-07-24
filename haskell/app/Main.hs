1439b976f3bb742a37572fe097b0c19a5ec15c87
True Ghiassi <true@ghiassitrio.co.uk>
2020-07-24 09:22:28 +0100

    Some experimentation with a kind of general framework to make the structure Elm-like.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -84,11 +84,6 @@ tcpReceive socket = do
 
         Right len -> do
             rawMsg <- Tcp.recv socket len
-            case decodeTcpMsg rawMsg of
-                Left err ->
-                    onTcpReceiveErr err socket
-
-                Right msg -> do
                     Stm.atomically $ do
                         q <- inQ
                         Q.writeTQueue q (FromServerI msg)
@@ -830,8 +825,36 @@ sendMessage draftId recipient = do
 
 
 main :: IO ()
-main = do
-    _ <- C.forkIO tcpClient
-    _ <- C.forkIO uiServer
-
+main =
+    _ <- mainHelp InitS StartUpI
     return ()
+
+
+mainHelp :: State -> Input -> IO (Either T.Text State)
+mainHelp state input =
+    case update state input of
+        Left err ->
+            return $ Left err
+
+        Right ok ->
+    let
+        eitherResult = update state input
+    in case
+        newInput <- io output
+        mainHelp newState newInput
+
+
+data State = InitS
+
+data Input
+    = StartUpI    
+
+
+io :: Output -> IO Input
+io =
+    undefined
+
+
+update :: State -> Input -> (State, Output)
+update =
+    undefined
