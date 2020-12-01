10e5f21ef3164fdcbd6adbcfce75f3bf6f86ebe6
True Ghiassi <true@ghiassitrio.co.uk>
2020-12-01 13:13:10 +0000

    Added test for uint16 parser and encoder.

--- client/test/Test.hs
+++ client/test/Test.hs
@@ -4,6 +4,7 @@ import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import qualified Hedgehog as H
 import qualified Lib as L
+import qualified Data.Attoparsec.ByteString as P
 
 main :: IO ()
 main =
@@ -15,7 +16,21 @@ properties =
     Tasty.testGroup ""
     [ Th.testProperty "reverse" propReverse
     , Th.testProperty "parseRecipient" parseRecipient
+    , Th.testProperty "uint16parseDecode" uint16parseDecode
     ]
+
+
+
+uint16parseDecode :: H.Property
+uint16parseDecode =
+    H.property $ do
+        n <- H.forAll $ Gen.int (Range.constant 0 65535)
+        case P.parseOnly L.uint16P (L.encodeUint16 n) of
+            Left _ ->
+                H.failure
+
+            Right n1 ->
+                n H.=== n1
 
 
 parseRecipient :: H.Property
