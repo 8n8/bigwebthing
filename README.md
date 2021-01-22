34682e2b50aa3b0309e192bdec3d09f21a163e28
True Ghiassi <true@ghiassitrio.co.uk>
2021-01-22 15:01:24 +0000

    Main part of Go 'read' crypto code is done.

--- README.md
+++ README.md
@@ -52,3 +52,29 @@ Public file containing sequence of KKs, where a KK is one of:
 		1 byte: 2
 		24 bytes: message
 		16 bytes: crypto overhead
+
+# Client process
+
+1. parse messages in public file, into a sequence of KK messages
+2. parse secret file
+3. sort the KK messages into sessions, where each session is one of:
+	sending
+		KK1
+		KK1 KK2
+		KK1 KK2 Transport
+	receiving
+		KK1
+		KK1 KK2
+		KK1 Kk2 Transport
+
+## Read
+
+It picks out all the sessions, both sending and receiving, that have a transport message, deciphers the messages, and prints them out.
+
+## Write
+
+It chooses a sending session that has a complete round trip but no transport, and uses it to encrypt a new message. The new message is then appended to the public file.
+
+## Update
+
+It counts the number of unused sending sessions for each contact, and tops them up to a threshold value.
--- clientgo/client.go
+++ clientgo/client.go
@@ -6,6 +6,8 @@ import (
 	"github.com/flynn/noise"
 	"os"
 	"io/ioutil"
+	"crypto/rand"
+	"io"
 )
 
 func main() {
@@ -142,13 +144,6 @@ func readCache() (Cache, error) {
 	return cache, nil
 }
 
-type Noise interface {
-}
-
-type KK interface {
-	read_(UserId, []Noise, Secret) ([]Noise, error)
-}
-
 func requestMessage(cache Cache) error {
 	fmt.Println("requestMessage not implemented yet")
 	return nil
@@ -161,22 +156,320 @@ func downloadMessage(cache Cache) ([]byte, error) {
 
 const kk1_size = 48
 
-func parseKk1(raw []byte, lenraw int, pos int) (KK, int, error) {
-	if lenraw - pos < kk1_size {
-		return nil, pos, TooShortForKk1{lenraw: lenraw, pos: pos}
+type Session interface {
+	insert(Sessions) Sessions
+}
+
+func (k TransportRx) insert(sessions Sessions) Sessions {
+	sessions.transportRx = append(sessions.transportRx, k)
+	return sessions
+}
+
+// The session can be one of these types:
+// 
+// 0. not to me or from this contact
+// sending
+// 	1. KK1
+// 	2. KK1 KK2
+// receiving
+// 	3. KK1
+// 	4. KK1 KK2
+// 	5. KK1 KK2 Transport
+func makeSessionFor(
+	kk1 Kk1,
+	contact UserId,
+	kk2s []Kk2,
+	transports []KkTransport,
+	secrets Secrets) (Session, error) {
+
+	session, done, err := txSessions(
+		kk1,
+		contact,
+		kk2s,
+		transports,
+		secrets)
+	if done {
+		return session, err
 	}
 
-	return Kk1(raw[pos: pos + kk1_size]), pos + kk1_size, nil
+	return rxSessions(kk1, contact, kk2s, transports, secrets)
+}
+
+var cryptoAd []byte = []byte{100, 117, 182, 195, 110, 70, 39, 86, 128, 240}
+
+func makeRandomGen(secret Secret) io.Reader {
+	panic("makeRandomGen not implemented yet")
+	return rand.Reader
+}
+
+func initShake(
+	secret Secret,
+	contact UserId,
+	staticKeys noise.DHKey,
+	initiator bool) (*noise.HandshakeState, error) {
+
+	cipherSuite := noise.NewCipherSuite(
+		noise.DH25519,
+		noise.CipherChaChaPoly,
+		noise.HashBLAKE2s)
+
+	config := noise.Config{
+		CipherSuite: cipherSuite,
+		Random: makeRandomGen(secret),
+		Pattern: noise.HandshakeKK,
+		Initiator: initiator,
+		StaticKeypair: staticKeys,
+		PeerStatic: contact,
+	}
+
+	return noise.NewHandshakeState(config)
+}
+
+func initShakeRx(
+	secret Secret,
+	contact UserId,
+	staticKeys noise.DHKey) (*noise.HandshakeState, error) {
+
+	return initShake(secret, contact, staticKeys, false)
+}
+
+func initShakeTx(
+	secret Secret,
+	contact UserId,
+	staticKeys noise.DHKey) (*noise.HandshakeState, error) {
+
+	return initShake(secret, contact, staticKeys, true)
+}
+
+func txSessions(
+	kk1 Kk1,
+	contact UserId,
+	kk2s []Kk2,
+	transports []KkTransport,
+	secrets Secrets) (Session, bool, error) {
+
+	secret, ok := secrets.sending.get(kk1, contact)
+	if !ok {
+		return nil, false, nil
+	}
+
+	// So it's a KK1 that I sent out at some stage.
+
+	shake, err := initShakeTx(secret, contact, secrets.staticKeys)
+	if err != nil {
+		return nil, true, err
+	}
+
+	// Replicate the KK1.
+	replica, _, _, err := shake.WriteMessage([]byte{}, []byte{})
+	if err != nil {
+		return nil, true, err
+	}
+	if !bytesEqual(replica, kk1) {
+		return nil, true, CouldntReplicateKk1{}
+	}
+
+	// See if there is a KK2 that someone sent in response.
+	var cipher *noise.CipherState
+	var kk2 Kk2
+	for _, kk2 = range kk2s {
+		_, cipher, _, err = shake.ReadMessage([]byte{}, kk2)
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		// So there is no responding KK2.
+		return Kk1Tx{
+			theirId: contact,
+			secret: secret,
+		}, true, nil
+	}
+
+	// So there is a responding KK2.
+	var plain []byte
+	for _, transport := range transports {
+		plain, err = cipher.Decrypt(
+			[]byte{},
+			cryptoAd,
+			transport)
+		if err == nil {
+			// So I sent out a transport.
+			return TransportTx{
+				theirid: contact,
+				secret: secret,
+				kk2: kk2,
+				plain: plain,
+			}, true, nil
+		}
+	}
+
+	// So there is a KK1 and a KK2 but no transport.
+	return Kk1Kk2Tx{
+		theirid: contact,
+		secret: secret,
+		kk2: kk2,
+	}, true, nil
+}
+
+func (k Kk1Tx) insert(sessions Sessions) Sessions {
+	sessions.kk1Tx = append(sessions.kk1Tx, k)
+	return sessions
+}
+
+func (k Kk1Kk2Tx) insert(sessions Sessions) Sessions {
+	sessions.kk1kk2Tx = append(sessions.kk1kk2Tx, k)
+	return sessions
+}
+
+func makeSessionSecret() (Secret, error) {
+	panic("makeSessionSecret not implemented yet")
+	return *new(Secret), nil
+}
+
+func rxSessions(
+	kk1 Kk1,
+	contact UserId,
+	kk2s []Kk2,
+	transports []KkTransport,
+	secrets Secrets) (Session, error) {
+
+	secret, ok := secrets.receiving.get(kk1, contact)
+	if !ok {
+		tmpSecret, err := makeSessionSecret()
+		if err != nil {
+			return nil, err
+		}
+		shake, err := initShakeRx(
+			tmpSecret, contact, secrets.staticKeys)
+		if err != nil {
+			return nil, err
+		}
+		_, _, _, err = shake.ReadMessage([]byte{}, kk1)
+		if err == nil {
+			return Kk1Rx{
+				theirid: contact,
+				kk1: kk1,
+			}, nil
+		}
+		return NoSession{}, nil
+	}
+
+	shake, err := initShakeRx(secret, contact, secrets.staticKeys)
+	if err != nil {
+		return nil, err
+	}
+
+
+	_, _, _, err = shake.ReadMessage([]byte{}, kk1)
+	if err != nil {
+		return NoSession{}, nil
+	}
+
+	newKk2, _, cipher, err := shake.WriteMessage(
+		[]byte{}, []byte{})
+	if err != nil {
+		return nil, err
+	}
+
+	if !matchingKk2(newKk2, kk2s) {
+		return Kk1Rx{
+			theirid: contact,
+			kk1: kk1,
+		}, nil
+	}
+
+	var transport []byte
+	for _, transport = range transports {
+		_, err = cipher.Decrypt(
+			[]byte{}, cryptoAd, transport)
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return Kk1Kk2Rx{
+			kk1: kk1,
+			theirid: contact,
+			secret: secret,
+		}, nil
+	}
+
+	return TransportRx{
+		theirid: contact,
+		secret: secret,
+		kk1: kk1,
+		transport: transport,
+	}, nil
+}
+
+func bytesEqual(as []byte, bs []byte) bool {
+	if len(as) != len(bs) {
+		return false
+	}
+	for i, a := range as {
+		if a != bs[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func matchingKk2(newKk2 Kk2, oldKk2s []Kk2) bool {
+	for _, oldKk2 := range oldKk2s {
+		if bytesEqual(newKk2, oldKk2) {
+			return true
+		}
+	}
+	return false
+}
+
+type NoSession struct{}
+
+func (NoSession) insert(sessions Sessions) Sessions {
+	return sessions
+}
+
+func parseKk1(p Parser) (Parser, error) {
+	if p.lenraw - p.cursor < kk1_size {
+		return p, TooShortForKk1(p)
+	}
+
+	kk1 := Kk1(p.raw[p.cursor: p.cursor + kk1_size])
+	kk1s := append(p.public.kk1s, kk1)
+	return Parser{
+		raw: p.raw,
+		lenraw: p.lenraw,
+		cursor: p.cursor + kk1_size,
+		counter: p.counter,
+		public: Public{
+			kk1s: kk1s,
+			kk2s: p.public.kk2s,
+			transports: p.public.transports,
+		},
+	}, nil
 }
 
 const kk2_size = 48
 
-func parseKk2(raw []byte, lenraw int, pos int) (KK, int, error) {
-	if lenraw - pos < kk2_size {
-		return nil, pos, TooShortForKk2{lenraw: lenraw, pos: pos}
+func parseKk2(p Parser) (Parser, error) {
+	if p.lenraw - p.cursor < kk2_size {
+		return p, TooShortForKk2(p)
 	}
 
-	return Kk2(raw[pos: pos + kk2_size]), pos + kk2_size, nil
+	kk2 := Kk2(p.raw[p.cursor: p.cursor + kk2_size])
+	kk2s := append(p.public.kk2s, kk2)
+	return Parser{
+		raw: p.raw,
+		lenraw: p.lenraw,
+		cursor: p.cursor + kk2_size,
+		counter: p.counter,
+		public: Public{
+			kk1s: p.public.kk1s,
+			kk2s: kk2s,
+			transports: p.public.transports,
+		},
+	}, nil
 }
 
 const kk_transport_size = 40
@@ -185,34 +478,13 @@ type KkTransport []byte
 
 type Kk1 []byte
 
-// A KK1 is one of:
-//
-// 1. new, sent to me
-//     + make a new KK2 in response
-//
-// 2. old, sent to me
-//     + make the Noise state
-//
-// 3. sent by me
-//     + make the Noise state
-//
-// 4. sent by someone else
-//	+ ignore
-//
-func (k Kk1) read_(contact UserId, noises []Noise, secret Secret) ([]Noise, error) {
-	session, known_kk := secret.receiving.get(k)
-	if known_kk {
-		// So it is an old one I have responded to.
-		replica_kk1, noise := makeKk1(
-			session.secret, contact, secret.staticKeys)
-		if replica_kk1 != k {
-			return noises, CouldntReplicateKk1{}
-		}
-	}
-}
+func makeConfig(
+	secret []byte,
+	userId UserId,
+	staticKeys noise.DHKey) noise.Config {
 
-func makeKk1(secret []byte, contact UserId) (Kk1, Noise) {
-	config := makeConfig(secret, contact)
+	panic("makeConfig not implemented yet")
+	return *new(noise.Config)
 }
 
 type CouldntReplicateKk1 struct{}
@@ -223,53 +495,66 @@ func (CouldntReplicateKk1) Error() string {
 
 type Kk2 []byte
 
-type TooShortForKk1 struct {
-	lenraw int
-	pos int
-}
+type TooShortForKk1 Parser
 
-type TooShortForKk2 struct {
-	lenraw int
-	pos int
-}
+type TooShortForKk2 Parser
 
 func (t TooShortForKk2) Error() string {
-	return fmt.Sprintf("bad 'public' file: failed parsing KK2: parser position %d, but length is %d", t.pos, t.lenraw)
+	return fmt.Sprintf("bad 'public' file: failed parsing KK2: parser position %d, but length is %d", t.cursor, t.lenraw)
 }
 
-type TooShortForTransport struct {
-	lenraw int
-	pos int
-}
+type TooShortForTransport Parser
 
 func (t TooShortForTransport) Error() string {
-	return fmt.Sprintf("bad 'public' file: failed parsing KK transport: parser position &d, but length is &d", t.pos, t.lenraw)
+	return fmt.Sprintf("bad 'public' file: failed parsing KK transport: parser position &d, but length is &d", t.cursor, t.lenraw)
 }
 
 func (t TooShortForKk1) Error() string {
-	return fmt.Sprintf("bad 'public' file: failed parsing KK1: parser position %d, but length is %d", t.pos, t.lenraw)
+	return fmt.Sprintf("bad 'public' file: failed parsing KK1: parser position %d, but length is %d", t.cursor, t.lenraw)
 }
 
-func parseKkTransport(raw []byte, lenraw int, pos int) (KK, int, error) {
-	if lenraw - pos < kk_transport_size {
-		return nil, pos, TooShortForTransport{lenraw: lenraw, pos: pos}
+func parseKkTransport(p Parser) (Parser, error) {
+	if p.lenraw - p.cursor < kk_transport_size {
+		return p, TooShortForTransport(p)
 	}
-	return KkTransport(raw[pos: pos + kk_transport_size]), pos + kk_transport_size, nil
+	transport := KkTransport(
+		p.raw[p.cursor: p.cursor + kk_transport_size])
+	transports := append(p.public.transports, transport)
+	return Parser{
+		raw: p.raw,
+		lenraw: p.lenraw,
+		cursor: p.cursor + kk_transport_size,
+		counter: p.counter,
+		public: Public{
+			kk1s: p.public.kk1s,
+			kk2s: p.public.kk2s,
+			transports: transports,
+		},
+	}, nil
 }
 
-func parseKk(raw []byte, lenraw int, pos int) (KK, int, error) {
-	indicator := raw[pos]
-	pos++
+type Parser struct {
+	raw []byte
+	lenraw int
+	cursor int
+	counter int
+	public Public
+}
+
+func parseKk(p Parser) (Parser, error) {
+
+	indicator := p.raw[p.cursor]
+	p.cursor++
 
 	switch indicator {
 	case 0:
-		return parseKk1(raw, lenraw, pos)
+		return parseKk1(p)
 	case 1:
-		return parseKk2(raw, lenraw, pos)
+		return parseKk2(p)
 	case 2:
-		return parseKkTransport(raw, lenraw, pos)
+		return parseKkTransport(p)
 	}
-	return nil, pos, BadKkIndicator(indicator)
+	return p, BadKkIndicator(indicator)
 }
 
 type BadKkIndicator int
@@ -286,53 +571,61 @@ func (NoPublicFile) Error() string {
 	return "need file named 'public'"
 }
 
-func readPublic() ([]KK, error) {
+func initParser(raw []byte) Parser {
+	return Parser{
+		raw: raw,
+		lenraw: len(raw),
+		cursor: 0,
+		counter: 0,
+		public: Public{
+			kk1s: make([]Kk1, 0),
+			kk2s: make([]Kk2, 0),
+			transports: make([]KkTransport, 0),
+		},
+	}
+}
+
+func getPublic() (Public, error) {
 	raw, err := ioutil.ReadFile(publicPath)
 	if os.IsNotExist(err) {
-		return []KK{}, NoPublicFile{}
+		return *new(Public), NoPublicFile{}
 	}
 	if err != nil {
-		return []KK{}, err
+		return *new(Public), err
 	}
 
-	kks := make([]KK, 0)
-	pos := 0
-	lenraw := len(raw)
-	for pos < lenraw {
-		kk, pos, err := parseKk(raw, lenraw, pos)
+	parser := initParser(raw)
+	for ; parser.cursor < parser.lenraw; parser.counter++ {
+		parser, err = parseKk(parser)
 		if err != nil {
-			return kks, err
+			return parser.public, err
 		}
-		kks = append(kks, kk)
 	}
-	return kks, nil
+	return parser.public, nil
 }
 
 const kk1Size = 48
 
-type Sessions interface {
-	insert(Kk1, Session)
+type SessionSecrets interface {
+	insert(Kk1, UserId, Secret)
 	remove(Kk1)
-	get(Kk1) (Session, bool)
+	get(Kk1, UserId) (Secret, bool)
 }
 
-type Session struct {
-	theirId UserId
-	secret []byte
-}
+type Secret []byte
 
-type Secret struct {
+type Secrets struct {
 	staticKeys noise.DHKey
 	contacts []UserId
-	sending Sessions
-	receiving Sessions
+	sending SessionSecrets
+	receiving SessionSecrets
 }
 
 const secretPath = "secret"
 
-func parseSessions(raw []byte, pos int) (Sessions, int, error) {
+func parseSessionSecrets(raw []byte, pos int) (SessionSecrets, int, error) {
 	panic("parseSessions not implemented yet")
-	return *new(Sessions), 0, nil
+	return nil, 0, nil
 }
 
 func parseContacts(raw []byte, pos int) ([]UserId, int, error) {
@@ -345,9 +638,9 @@ func parseStaticKeys(raw []byte, pos int) (noise.DHKey, int, error) {
 	return *new(noise.DHKey), 0, nil
 }
 
-func parseSecret(raw []byte) (Secret, error) {
+func parseSecrets(raw []byte) (Secrets, error) {
 	pos := 0
-	var secret Secret
+	var secret Secrets
 
 	staticKeys, pos, err := parseStaticKeys(raw, pos)
 	if err != nil {
@@ -359,12 +652,12 @@ func parseSecret(raw []byte) (Secret, error) {
 		return secret, err
 	}
 
-	sending, pos, err := parseSessions(raw, pos)
+	sending, pos, err := parseSessionSecrets(raw, pos)
 	if err != nil {
 		return secret, err
 	}
 
-	receiving, pos, err := parseSessions(raw, pos)
+	receiving, pos, err := parseSessionSecrets(raw, pos)
 	if err != nil {
 		return secret, err
 	}
@@ -375,7 +668,7 @@ func parseSecret(raw []byte) (Secret, error) {
 			lenraw: len(raw)}
 	}
 
-	return Secret {
+	return Secrets {
 		staticKeys: staticKeys,
 		contacts: contacts,
 		sending: sending,
@@ -392,49 +685,213 @@ func (e ExpectSecretEnd) Error() string {
 	return fmt.Sprintf("bad secret file: expecting file end at position %d, but length is %d", e.pos, e.lenraw)
 }
 
-func makeSecret() (Secret, error) {
+func makeSecrets() (Secrets, error) {
 	panic("makeSecret not done yet")
-	return *new(Secret), nil
+	return *new(Secrets), nil
 }
 
-func readSecret() (Secret, error) {
+func getSecrets() (Secrets, error) {
 	raw, err := ioutil.ReadFile(secretPath)
 	if os.IsNotExist(err) {
-		return makeSecret()
+		return makeSecrets()
 	}
 	if err != nil {
-		return *new(Secret), err
+		return *new(Secrets), err
 	}
-	return parseSecret(raw)
+	return parseSecrets(raw)
 }
 
-func processKk(kk KK, noises []Noise, secret Secret) ([]Noise, error) {
-	for _, contact := range secret.contacts {
-		noises, err := kk.read_(contact, noises, secret)
-		if err != nil {
-			return noises, err
-		}
+type TransportSession struct {
+	theirid UserId
+	secret Secret
+	kk1 Kk1
+	kk2 Kk2
+	transport KkTransport
+}
+
+type Kk1Kk2Session struct {
+	theirid UserId
+	secret Secret
+	kk1 Kk1
+	kk2 Kk2
+}
+
+func (k Kk1Rx) insert(sessions Sessions) Sessions {
+	sessions.kk1Rx = append(sessions.kk1Rx, k)
+	return sessions
+}
+
+type Kk1TxSession struct {
+	theirId UserId
+	secret Secret
+	kk1 Kk1
+}
+
+type Sessions struct {
+	transportRx []TransportRx
+	transportTx []TransportTx
+	kk1kk2Rx []Kk1Kk2Rx
+	kk1kk2Tx []Kk1Kk2Tx
+	kk1Rx []Kk1Rx
+	kk1Tx []Kk1Tx
+}
+
+func (k TransportTx) insert(sessions Sessions) Sessions {
+	sessions.transportTx = append(sessions.transportTx, k)
+	return sessions
+}
+
+type TransportTx struct {
+	theirid UserId
+	secret Secret
+	kk2 []byte
+	plain []byte
+}
+
+type TransportRx struct {
+	theirid UserId
+	secret Secret
+	kk1 []byte
+	transport []byte
+}
+
+type Kk1Kk2Rx struct {
+	kk1 []byte
+	theirid UserId
+	secret Secret
+}
+
+func (k Kk1Kk2Rx) insert(sessions Sessions) Sessions {
+	sessions.kk1kk2Rx = append(sessions.kk1kk2Rx, k)
+	return sessions
+}
+
+type Kk1Kk2Tx struct {
+	theirid UserId
+	secret Secret
+	kk2 []byte
+}
+
+type Kk1Rx struct {
+	theirid UserId
+	kk1 []byte
+}
+
+type Kk1Tx struct {
+	theirId UserId
+	secret Secret
+}
+
+func addSessions(s1 Sessions, s2 Sessions) Sessions {
+	return Sessions{
+		transportRx: append(s1.transportRx, s2.transportRx...),
+		kk1kk2Rx: append(s1.kk1kk2Rx, s2.kk1kk2Rx...),
+		kk1kk2Tx: append(s1.kk1kk2Tx, s2.kk1kk2Tx...),
+		kk1Rx: append(s1.kk1Rx, s2.kk1Rx...),
+		kk1Tx: append(s1.kk1Tx, s2.kk1Tx...),
 	}
-	return noises, nil
+}
+
+func initSessions() Sessions {
+	return Sessions{
+		transportRx: make([]TransportRx, 0),
+		transportTx: make([]TransportTx, 0),
+		kk1kk2Rx: make([]Kk1Kk2Rx, 0),
+		kk1kk2Tx: make([]Kk1Kk2Tx, 0),
+		kk1Rx: make([]Kk1Rx, 0),
+		kk1Tx: make([]Kk1Tx, 0),
+	}
+}
+
+type Public struct {
+	kk1s []Kk1
+	kk2s []Kk2
+	transports []KkTransport
+}
+
+func makeSessionsFor(
+	contact UserId,
+	public Public,
+	secrets Secrets) (Sessions, error) {
+
+	sessions := initSessions()
+	for _, kk1 := range public.kk1s {
+		session, err := makeSessionFor(
+			kk1,
+			contact,
+			public.kk2s,
+			public.transports,
+			secrets)
+		if err != nil {
+			return sessions, err
+		}
+		sessions = session.insert(sessions)
+	}
+	return sessions, nil
+}
+
+func makeSessions(public Public, secrets Secrets) (Sessions, error) {
+	sessions := initSessions()
+	for _, contact := range secrets.contacts {
+		newSessions, err := makeSessionsFor(
+			contact, public, secrets)
+		if err != nil {
+			return sessions, err
+		}
+		sessions = addSessions(sessions, newSessions)
+	}
+	return sessions, nil
+}
+
+func getSessions(secrets Secrets) (Sessions, error) {
+	public, err := getPublic()
+	if err != nil {
+		return *new(Sessions), err
+	}
+	return makeSessions(public, secrets)
+}
+
+func showTransportRx(t TransportRx, staticKeys noise.DHKey) (string, error) {
+	shake, err := initShakeRx(t.secret, t.theirid, staticKeys)
+	if err != nil {
+		return "", err
+	}
+
+	_, _, _, err = shake.ReadMessage([]byte{}, t.kk1)
+	if err != nil {
+		return "", err
+	}
+
+	_, _, cipher, err := shake.WriteMessage([]byte{}, []byte{})
+	if err != nil {
+		return "", err
+	}
+
+	plain, err := cipher.Decrypt([]byte{}, cryptoAd, t.transport)
+	if err != nil {
+		return "", err
+	}
+	return string(plain), nil
 }
 
 func (Read_) run() error {
-	secret, err := readSecret()
+	secrets, err := getSecrets()
 	if err != nil {
 		return err
 	}
 
-	public, err := readPublic()
+	sessions, err := getSessions(secrets)
 	if err != nil {
 		return err
 	}
 
-	noises := make([]Noise, 0)
-	for _, kk := range public {
-		noises, err = processKk(kk, noises, secret)
+	for _, transport := range sessions.transportRx {
+		pretty, err := showTransportRx(
+			transport, secrets.staticKeys)
 		if err != nil {
 			return err
 		}
+		fmt.Println(pretty)
 	}
 
 	return nil
