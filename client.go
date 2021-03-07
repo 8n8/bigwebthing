37e741e13a9a73de781ef5f7aed15fb004056ee1
True Ghiassi <true@ghiassitrio.co.uk>
2021-03-07 20:51:04 +0000

    Hello world GUI using WebView.

--- client.go
+++ client.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/webview/webview"
 )
 
 func main() {
-	fmt.Println("hey")
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("BigWebThing")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://bbc.co.uk")
+	w.Run()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module client
 
 go 1.15
+
+require github.com/webview/webview v0.0.0-20210216142346-e0bfdf0e5d90
