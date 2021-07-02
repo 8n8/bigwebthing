9683c0d917452feec70f760b048b11a789b7d119
True Ghiassi <true@ghiassitrio.co.uk>
2021-07-02 14:36:22 +0100

    Successfully drawn a white triangle on a black background.

--- opengl/main.c
+++ opengl/main.c
@@ -3,6 +3,23 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+// Shader sources
+const GLchar* vertexSource = 
+"#version 150 core\n"
+"in vec2 position;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(position, 0.0, 1.0);\n"
+"}";
+
+const GLchar* fragmentSource = 
+"#version 150 core\n"
+"out vec4 outColor;\n"
+"void main()\n"
+"{\n"
+"	outColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
+"}";
+
 int main() {
 	glfwInit();
 
@@ -20,15 +37,84 @@ int main() {
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer);
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	
+	GLfloat vertices[] = {
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLint status;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	if (status != GL_TRUE) {
+		char buffer[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+		printf("vertex shader didn't compile: %s\n", buffer);
+		return 1;
+	}
+	
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	if (status != GL_TRUE) {
+		printf("fragment shader didn't compile\n");
+		return 1;
+	}
+	
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
 
-	printf("%u\n", vertexBuffer);
+	GLenum err = glGetError();
+	if (err != GL_NO_ERROR) {
+		printf("GL error: %d", err);
+		return err;
+	}
+
+	glUseProgram(shaderProgram);
+
+	// Specify the layout of the vertex data
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	err = glGetError();
+	if (err != GL_NO_ERROR) {
+		printf("GL eror: %d", err);
+		return err;
+	}
 
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glDeleteBuffers(1, &vbo);
+
+	glDeleteVertexArrays(1, &vao);
 
 	glfwTerminate();
 }
