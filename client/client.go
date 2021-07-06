fbccb22666304efc17b68e2c3a0508da79ec5749
True Ghiassi <true@ghiassitrio.co.uk>
2021-07-06 22:27:40 +0100

    Added function for removing a contact.

--- client/client.go
+++ client/client.go
@@ -26,25 +26,6 @@ func AddContact(publicId, localId string) {
 
 func addContactErr(publicId, localId string) error {
 	staticKey := getStaticKey()
-	handshake, err := noise.NewHandshakeState(noise.Config{
-		CipherSuite: noise.NewCipherSuite(
-			noise.DH25519,
-			noise.CipherChaChaPoly,
-			noise.HashBLAKE2s),
-		Random:        rand.Reader,
-		Pattern:       noise.HandshakeXK,
-		Initiator:     true,
-		StaticKeypair: staticKey,
-		PeerStatic:    serverStaticKey,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	xk1, _, _, err := handshake.WriteMessage([]byte{}, []byte{})
-	if err != nil {
-		return err
-	}
 
 	conn, err := net.Dial("tcp", serverUrl)
 	if err != nil {
@@ -52,36 +33,13 @@ func addContactErr(publicId, localId string) error {
 	}
 	defer conn.Close()
 
-	_, err = conn.Write(xk1)
+	var tx, rx *noise.CipherState
+	err = doHandshake(conn, staticKey, tx, rx)
 	if err != nil {
 		return err
 	}
 
-	xk2 := make([]byte, xk2Len)
-	n, err := conn.Read(xk2)
-	if err != nil {
-		return err
-	}
-	if n != xk2Len {
-		return NotEnoughXk2Bytes(n)
-	}
-
-	_, _, _, err = handshake.ReadMessage([]byte{}, xk2)
-	if err != nil {
-		return err
-	}
-
-	xk3, tx, _, err := handshake.WriteMessage([]byte{}, []byte{})
-	if err != nil {
-		return err
-	}
-
-	_, err = conn.Write(xk3)
-	if err != nil {
-		return err
-	}
-
-	addContact := make([]byte, 1+addContactLen)
+	addContact := make([]byte, 1+contactLen)
 	addContact[0] = 1
 	addContact[1] = addContactId
 
@@ -92,9 +50,8 @@ func addContactErr(publicId, localId string) error {
 	copy(addContact[1+1+4:], contacter)
 	copy(addContact[1+1+4+32:], staticKey.Public)
 
-	encrypted := make(
-		[]byte, 2+1, 2+1+addContactLen+authTagLen)
-	copy(encrypted, encodeUint16(1+addContactLen+authTagLen))
+	encrypted := make([]byte, 2+1, 2+1+contactLen+authTagLen)
+	copy(encrypted, encodeUint16(1+contactLen+authTagLen))
 
 	_, err = tx.Encrypt(encrypted, ad, addContact)
 	if err != nil {
@@ -167,6 +124,7 @@ func (n NotEnoughXk2Bytes) Error() string {
 
 func saveNewContact(contacter []byte, localId string) error {
 	db := setupDb()
+	defer db.Close()
 	_, err := db.Exec(addContactSql, contacter, localId)
 	return err
 }
@@ -179,12 +137,71 @@ var ad = []byte{
 	248, 0, 56, 173, 54, 51, 61, 107, 170, 15, 53, 205, 36, 4, 67, 156, 186, 159, 44, 196, 114, 221, 191, 251, 143, 167, 73, 252, 139, 144, 60, 66,
 }
 
-const addContactLen = 69
+const contactLen = 69
 const authTagLen = 16
 const addContactId = 4
 
-func RemoveContact(publicId string, localId string) {
+func RemoveContact(publicId, localId string) {
+	err := removeContactErr(publicId, localId)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 }
+
+func removeContactErr(publicId, localId string) error {
+	staticKey := getStaticKey()
+
+	conn, err := net.Dial("tcp", serverUrl)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	var tx, rx *noise.CipherState
+	err = doHandshake(conn, staticKey, tx, rx)
+	if err != nil {
+		return err
+	}
+
+	removeContact := make([]byte, 1+contactLen)
+	removeContact[0] = 1
+	removeContact[1] = removeContactId
+
+	contacter, err := base64.RawURLEncoding.DecodeString(publicId)
+	if err != nil {
+		return err
+	}
+	copy(removeContact[1+1+4:], contacter)
+	copy(removeContact[1+1+4+32:], staticKey.Public)
+
+	encrypted := make([]byte, 2+1, 2+1+contactLen+authTagLen)
+	copy(encrypted, encodeUint16(1+contactLen+authTagLen))
+
+	_, err = tx.Encrypt(encrypted, ad, removeContact)
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write(encrypted)
+	if err != nil {
+		return err
+	}
+
+	return saveRemoveContact(localId)
+}
+
+const removeContactSql = `
+DELETE FROM friendlyname WHERE name=?;
+`
+
+func saveRemoveContact(localId string) error {
+	db := setupDb()
+	defer db.Close()
+	_, err := db.Exec(removeContactSql, localId)
+	return err
+}
+
+const removeContactId = 5
 
 func Send(path string, recipient string) {
 }
@@ -196,6 +213,61 @@ func View() {
 }
 
 func MyId() {
+}
+
+func doHandshake(
+	conn net.Conn,
+	staticKey noise.DHKey,
+	tx *noise.CipherState,
+	rx *noise.CipherState) error {
+
+	handshake, err := noise.NewHandshakeState(noise.Config{
+		CipherSuite: noise.NewCipherSuite(
+			noise.DH25519,
+			noise.CipherChaChaPoly,
+			noise.HashBLAKE2s),
+		Random:        rand.Reader,
+		Pattern:       noise.HandshakeXK,
+		Initiator:     true,
+		StaticKeypair: staticKey,
+		PeerStatic:    serverStaticKey,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	xk1, _, _, err := handshake.WriteMessage([]byte{}, []byte{})
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write(xk1)
+	if err != nil {
+		return err
+	}
+
+	xk2 := make([]byte, xk2Len)
+	n, err := conn.Read(xk2)
+	if err != nil {
+		return err
+	}
+	if n != xk2Len {
+		return NotEnoughXk2Bytes(n)
+	}
+
+	_, _, _, err = handshake.ReadMessage([]byte{}, xk2)
+	if err != nil {
+		return err
+	}
+
+	xk3, tx, rx, err := handshake.WriteMessage([]byte{}, []byte{})
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write(xk3)
+
+	return err
 }
 
 const dhlen = 32
