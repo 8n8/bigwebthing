f2b31d024c5c766ab04de80e72d0c7cffbe869f0
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-28 18:24:48 +0100

    Boilerplate

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/74d017edb6717ad76d38edc02ad3210d4ad66b96.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python39
+    pkgs.python39Packages.flake8
+    pkgs.python39Packages.pytest
+    pkgs.python39Packages.noiseprotocol
+    pkgs.python39Packages.black
+  ];
+}
